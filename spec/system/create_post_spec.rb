bda4039b82f56d423c6b893d32c9542ce23b1a14
mattand <mjande@umich.edu>
2022-09-05 15:02:19 -0400

    Add rubocop with rails and rspec

--- spec/system/create_post_spec.rb
+++ spec/system/create_post_spec.rb
@@ -1,37 +1,42 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'create post spec', type: :system do
+  let(:user) { create(:user) }
+
   before do
-    @user = create(:user)
-    sign_in @user
+    sign_in user
     visit posts_url
-    click_on "New post"
+    click_on 'New post'
   end
-  
+
   context 'with valid inputs' do
     it 'successfully saves post' do
-      within("h1") do
-        expect(page).to have_content("New Post")
+      within('h1') do
+        expect(page).to have_content('New Post')
       end
-      
-      fill_in "post_body",	with: "Test" 
-      click_on "Create Post"
-      
-      expect(page).to have_content("Posts")
-      expect(page).to have_content("Test")
+
+      fill_in 'post_body',	with: 'Test'
+      click_on 'Create Post'
+
+      expect(page).to have_content('Posts')
+      expect(page).to have_content('Test')
     end
   end
 
   context 'with invalid inputs' do
     it 'does not save and displays error messages' do
-      within("h1") do
-        expect(page).to have_content("New Post")
+      within('h1') do
+        expect(page).to have_content('New Post')
       end
 
-      click_on "Create Post"
+      click_on 'Create Post'
 
-      expect(page).to have_content("New Post")
+      expect(page).to have_content('New Post')
       expect(page).to have_content("Body can't be blank")
     end
   end
 end
+
+"test"
