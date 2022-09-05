a91004eb8a338a9290690b735b4d86273686e73c
mattand <mjande@umich.edu>
2022-09-05 11:48:52 -0400

    Add devise configuration with turbo

---
+++ app/controllers/turbo_devise_user_controller.rb
@@ -0,0 +1,19 @@
+# Configuration to allow devise to work with turbo. Solution stolen from Efo Coder at https://dev.to/efocoder/how-to-use-devise-with-turbo-in-rails-7-9n9
+class TurboDeviseUserController < ApplicationController
+  class Responder < ActionController::Responder
+    def to_turbo_stream
+      controller.render(options.merge(formats: :html))
+          rescue ActionView::MissingTemplate => error
+      if get? 
+        raise error
+      elsif has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      else
+        redirect_to navigation_location
+      end
+    end
+  end
+
+  self.responder = Responder
+  respond_to :html, :turbo_stream
+end
