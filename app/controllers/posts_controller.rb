5c3a7e5fb26c156d7c3d2e6e1a5011d7ac7c6b48
mattand <mjande@umich.edu>
2022-09-05 13:31:45 -0400

    Add creating posts feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,25 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:success] = "Post successfully created"
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
