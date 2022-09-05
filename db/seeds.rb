af2737a529542b67d69bed05b158ee175791c10f
mattand <mjande@umich.edu>
2022-09-05 13:12:11 -0400

    Add example users to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create some default users
+User.create(username: 'Chris Smith', email: 'chris@example.com', password: 'password')
+User.create(username: 'Mary Murphy', email: 'mary@example.com', password: 'password')
+User.create(username: 'James Jameson', email: 'james@example.com', password: 'password')
