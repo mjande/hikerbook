a0ca3899c2cbe0bc4634b0dfd6508bc6b20640f5
mattand <mjande@umich.edu>
2022-09-06 08:47:00 -0400

    Add specs for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let(:user) { create(:user) }
+
+  it 'is valid with valid attributes' do
+    post = user.posts.build(body: 'test')
+    expect(post).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    expect(described_class.new).not_to be_valid
+  end
+
+  it 'is not valid without a user' do
+    post = described_class.new(body: 'test')
+    expect(post).not_to be_valid
+  end
+
+  it 'belongs to a user' do
+    post = user.posts.build(body: 'test')
+    expect(post.user).to eq(user)
+  end
 end
