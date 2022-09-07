ea911053d4b038e68bcc7f0ba529e5e3f52567fa
mattand <mjande@umich.edu>
2022-09-07 15:08:48 -0400

    Add time stamps to post partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[edit update destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.order('created_at DESC')
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,11 @@ class Post < ApplicationRecord
   validates :trail, presence: true
   validates :park, presence: true
   validates :description, presence: true
-  
 
   belongs_to :user
+
+  def time
+    time_zone = ActiveSupport::TimeZone['Eastern Time (US & Canada)']
+    created_at.in_time_zone(time_zone).strftime('%a, %B %e, %Y at %I:%M%p')
+  end
 end
