aef7685878cb419359230c1dbdcd26325715d815
mattand <mjande@umich.edu>
2022-09-08 09:53:52 -0400

    Create users#index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: %i[show index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :posts
