227575e3363d8ff3169db8b2aa4e24dac3916eb1
mattand <mjande@umich.edu>
2022-09-21 15:21:54 -0400

    Add comment#create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,35 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comments
 
-  def index
+  def index; end
+
+  def new
     @comments = @post.comments.all
+    @comment = Comment.new(post_id: params[:post_id])
+  end
+
+  def create
+    @comment = @post.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render 'new'
+    end
   end
 
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comments
+    @comments = @post.comments.all
+  end
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :body)
   end
 end
