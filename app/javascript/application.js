069303d689d3a5a2573d2ad1177268880e3b2b44
mattand <mjande@umich.edu>
2022-09-22 10:28:14 -0400

    Add turbo for comment#create

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "controllers"
 
 // Temporary script for deactivating Turbo across the whole app (for testing compatibility with old browsers)
-import { Turbo } from '@hotwired/turbo-rails'
-Turbo.session.drive = false
+// import { Turbo } from '@hotwired/turbo-rails'
+// Turbo.session.drive = false
