1360443639c8dfc7c62276fe6300820f81123647
mattand <mjande@umich.edu>
2022-09-26 13:47:27 -0400

    Add integration with Gravatar for profile pictures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def render_turbo_stream_flash_messages
     turbo_stream.update 'flash', partial: 'layouts/flash'
   end
+
+  # https://stackoverflow.com/questions/14361952/rails-gravatar-helper-method
+  def avatar_url(user, size)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=retro"
+  end
 end
