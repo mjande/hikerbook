3b05fb5d408b1343cb11c3611dad658f849c155b
mattand <mjande@umich.edu>
2022-11-22 11:00:22 -0500

    Edit Stimulus controllers

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,8 +1,8 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
-export default class extends Controller { 
-  static targets = [ 'comment', 'showLink', 'hideLink' ]
-  static classes = [ 'hidden' ]
+export default class extends Controller {
+  static targets = ["comment", "showLink", "hideLink"];
+  static classes = ["hidden"];
 
   connect() {
     // Upon loading page, show only first three comments (keep the rest hidden)
@@ -13,12 +13,12 @@ export default class extends Controller {
 
     // Upon loading page, if there more than three comments, show the link to
     // reveal more comments
-    if(this.commentTargets.length <= 3) {
+    if (this.commentTargets.length <= 3) {
       this.showLinkTarget.classList.add(this.hiddenClass);
     }
 
     // Toggle status to change behavior of broadcasted comments
-    this.status = 'hidden'
+    this.status = "hidden";
   }
 
   show() {
@@ -32,14 +32,14 @@ export default class extends Controller {
     this.hideLinkTarget.classList.remove(this.hiddenClass);
 
     // Toggle status to change behavior of broadcasted comments
-    this.status = 'shown'
+    this.status = "shown";
   }
 
   hide() {
-    // Determine additional comments (which will be outdated if new comments 
+    // Determine additional comments (which will be outdated if new comments
     // have been broadcasted in)
     this.addtlComments = this.commentTargets.slice(3);
-  
+
     // Hide only additional comments (besides first three)
     this.addtlComments.forEach((comment) => {
       comment.classList.add(this.hiddenClass);
@@ -48,19 +48,19 @@ export default class extends Controller {
     // Switch hide link with show link
     this.showLinkTarget.classList.remove(this.hiddenClass);
     this.hideLinkTarget.classList.add(this.hiddenClass);
-    
+
     // Toggle status to change behavior of broadcasted comments
-    this.status = 'hidden';
+    this.status = "hidden";
   }
 
   // Triggered when new comments are broadcasted in
   commentTargetConnected() {
     // If all comments are showing, show this one as well
-    if (this.status == 'shown' || this.commentTargets.length <= 3) {
-      this.element.querySelector('.hidden').classList.remove(this.hiddenClass);
+    if (this.status == "shown" || this.commentTargets.length <= 3) {
+      this.element.querySelector(".hidden").classList.remove(this.hiddenClass);
 
-    // If additional comments are hidden but there are more than three total, 
-    // reveal show link
+      // If additional comments are hidden but there are more than three total,
+      // reveal show link
     } else if (this.commentTargets.length > 3) {
       this.showLinkTarget.classList.remove(this.hiddenClass);
     }
@@ -73,9 +73,9 @@ export default class extends Controller {
       this.showLinkTarget.classList.add(this.hiddenClass);
       this.hideLinkTarget.classList.add(this.hiddenClass);
 
-      // Toggle status to hidden, so links are added properly after 3 or more 
+      // Toggle status to hidden, so links are added properly after 3 or more
       // comments are present
-      this.status = 'hidden';
+      this.status = "hidden";
     }
   }
 }
